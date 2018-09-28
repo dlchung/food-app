958536d3df089ea77aa5f101c5044e66e972b911
Dan Chung <danetix@gmail.com>
2018-09-28 12:16:30 -0400

    rating colors

--- src/components/SearchResultsItemRatings.js
+++ src/components/SearchResultsItemRatings.js
@@ -6,7 +6,9 @@ import { Card, Segment, Statistic } from 'semantic-ui-react'
 
 export default class SearchResultsItemRatings extends Component {
   state = {
-    yelpRating: "N/A"
+    yelpRating: "n/a",
+    foursquareRating: "n/a",
+    grubhubRating: "n/a"
   }
   componentDidMount() {
     this.getRatings()
@@ -18,14 +20,38 @@ export default class SearchResultsItemRatings extends Component {
     response.then(resp => this.setState(resp.data))
   }
 
+  statisticColor = (statistic) => {
+    if(statistic !== "n/a") {
+      const score = parseInt(statistic, 10)
+      if(score > 3.4) {
+        return "green"
+      } else if(score > 1.9) {
+        return "yellow"
+      } else {
+        return "red"
+      }
+    }
+    else {
+      return "grey"
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <Card.Description>
           <Segment basic textAlign="center">
-            <Statistic>
+            <Statistic color={this.statisticColor(this.state.yelpRating)}>
               <Statistic.Value>{this.state.yelpRating}</Statistic.Value>
               <Statistic.Label>Yelp</Statistic.Label>
+            </Statistic>
+            <Statistic color={this.statisticColor(this.state.foursquareRating)}>
+              <Statistic.Value>{this.state.foursquareRating}</Statistic.Value>
+              <Statistic.Label>Foursquare</Statistic.Label>
+            </Statistic>
+            <Statistic color={this.statisticColor(this.state.grubhubRating)}>
+              <Statistic.Value>{this.state.grubhubRating}</Statistic.Value>
+              <Statistic.Label>Grubhub</Statistic.Label>
             </Statistic>
           </Segment>
         </Card.Description>
