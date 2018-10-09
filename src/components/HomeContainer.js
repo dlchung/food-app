4288844b51b10bbe3d5d89b52a6895ac18dbf6ee
Dan Chung <danetix@gmail.com>
2018-10-09 12:31:31 -0400

    readme

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -13,17 +13,17 @@ export default class HomeContainer extends Component {
           <Grid centered>
             <Grid.Row>
               <Grid.Column width={5}>
-                <Divider section horizontal>Column 1</Divider>
+                <Divider section horizontal>Top Restaurants in Your Area</Divider>
                 <p>Praesent quis nisi non tellus pretium tempor. Nam risus enim, mattis id tellus sit amet, rhoncus congue justo. In pretium leo semper ipsum mattis, at dapibus orci varius.</p>
                 <p>Aliquam vulputate odio enim, ut facilisis nibh semper vel. Nulla non eros eget elit semper fermentum. Integer nibh eros, varius et dui non, pellentesque fringilla mi.</p>
               </Grid.Column>
               <Grid.Column width={5}>
-                <Divider section horizontal>Column 2</Divider>
+                <Divider section horizontal>Your Favorite Restaurants</Divider>
                 <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. In ornare purus at libero tempus egestas. Nunc eget egestas metus.</p>
                 <p>Proin tempus, libero sed fermentum scelerisque, mi ex lacinia neque, ac pellentesque sapien nulla aliquam arcu. Etiam ac finibus eros.</p>
               </Grid.Column>
               <Grid.Column width={5}>
-                <Divider section horizontal>Column 3</Divider>
+                <Divider section horizontal>Suggested Restaurants</Divider>
                 <p>Sed sit amet dui quis arcu auctor elementum. Etiam in nulla augue. Maecenas metus lorem, iaculis sed eros vel, varius suscipit arcu. Quisque ut aliquam nulla. Donec urna sapien, congue vitae tristique in, mollis sed nisi.</p>
                 <p>Quisque ullamcorper pulvinar nunc, et tempus nibh pharetra nec. Nam molestie lacinia pellentesque.</p>
               </Grid.Column>
