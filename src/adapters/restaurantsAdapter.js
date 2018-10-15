d413de8fa966dcb615bee37780c7cb61fcfb483d
Dan Chung <danetix@gmail.com>
2018-10-15 15:32:19 -0400

    modified query, changed params

--- src/adapters/restaurantsAdapter.js
+++ src/adapters/restaurantsAdapter.js
@@ -4,15 +4,19 @@ const API_URL = "http://localhost:3000/api"
 
 export const fetchAllRestaurants = () => {
   const body = {
-    "query": "{ allRestaurants { id name description restaurant_type street city state zipcode created_at updated_at google_places_id google_lat google_lng google_types third_party_rating_id phone phone_2 yelp_url foursquare_url googleplaces_url zomato_url yelp_id foursquare_id zomato_id } }"
+    "query": "{ allRestaurants { id name description restaurant_type street city state zipcode created_at updated_at google_places_id lat lng google_types third_party_rating_id phone phone_2 yelp_url foursquare_url googleplaces_url zomato_url yelp_id foursquare_id zomato_id } }"
   }
   return axios.post(API_URL, body)
 }
 
 export const fetchNearbyRestaurants = (latLng, keyword, radius = "1500") => {
-  const location = `${latLng.lat},${latLng.lng}`
-  const type = "restaurant"
-  const body = { location, radius, type, keyword }
+  // const location = `${latLng.lat},${latLng.lng}`
+  const lat = latLng.lat
+  const lng = latLng.lng
+  // const type = "restaurant"
+  const type = "restaurants, All"
+  const limit = 20
+  const body = { lat, lng, radius, type, keyword, limit }
   return axios.post(`${API_URL}/nearby`, body)
 }
 
